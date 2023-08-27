f3c94e23f9e0b499fb5816f4d4965ce040ae19ae
Saeed <appsaeed7@gmail.com>
2023-08-28 05:13:41 +0600

    update vite-config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
-import { defineConfig } from "vite";
+import { readFile, writeFile } from "fs/promises";
+import { resolve } from "path";
+import { defineConfig, loadEnv } from "vite";
 import solid from "vite-plugin-solid";
+
+const env = Object.assign(process.env, loadEnv("mock", process.cwd(), ""));
+
+const isdev = env.NODE_ENV === "development" ? true : false;
+
+console.log("check development:", isdev);
+//basepath
+const basename = isdev ? "" : env.VITE_BASENAME;
+
 export default defineConfig({
   plugins: [solid()],
   server: {
-    port: 3000,
+    port: Number(env.VITE_PORT) || 3000,
   },
-  base: "./",
+  base: basename,
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: "404-html",
+          async writeBundle(options) {
+            const indexHtmlPath = resolve(options.dir, "index.html");
+            const buildIndexHtmlPath = resolve(options.dir, "404.html");
+            try {
+              const indexHtmlContent = await readFile(indexHtmlPath, "utf-8");
+              await writeFile(buildIndexHtmlPath, indexHtmlContent);
+            } catch (error) {
+              console.error("Error duplicating index.html:", error);
+            }
+          },
+        },
+      ],
+    },
+  },
 });
