1177e5f99acf057c213a2ae24807e704b5b3fe02
Saeed <appsaeed7@gmail.com>
2023-09-04 22:53:43 +0600

    pwa fix assets

--- vite.config.ts
+++ vite.config.ts
@@ -10,49 +10,73 @@ try {
 
 const manifest: Partial<ManifestOptions> = {
   name: env.VITE_NAME || "app name",
-  short_name: env.VITE_NAME || "App short name",
+  short_name: "Appsaeed",
   description: env.VITE_DESCRIPTION || "App description",
-  theme_color: env.VITE_THEME_COLOR || "#ffffff",
-  background_color: "##1e293b",
+  theme_color: "#0f172a",
+  background_color: "#1e293b",
   display: "standalone",
   scope: "/",
   start_url: "/",
   icons: [
     {
-      type: "image/png",
-      src: "/icon-512x512.png",
-      sizes: "512x512",
-      purpose: "any",
-    },
-    {
-      type: "image/png",
-      src: "/icon-384x384.png",
-      sizes: "384x384",
-      purpose: "any",
-    },
-    {
-      type: "image/png",
-      src: "/icon-256x256.png",
-      sizes: "256x256",
-      purpose: "any",
-    },
-    {
-      type: "image/png",
-      src: "/icon-192x192.png",
-      sizes: "192x192",
-      purpose: "any",
-    },
-    {
-      type: "image/png",
-      src: "/icon-32x32.png",
-      sizes: "32x32",
-      purpose: "any",
-    },
-    {
-      type: "image/png",
       src: "/icon-16x16.png",
       sizes: "16x16",
-      purpose: "any",
+      type: "image/png",
+    },
+    {
+      src: "/icon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      src: "/icon-72x72.png",
+      sizes: "72x72",
+      type: "image/png",
+    },
+    {
+      src: "/icon-96x96.png",
+      sizes: "96x96",
+      type: "image/png",
+    },
+    {
+      src: "/icon-128x128.png",
+      sizes: "128x128",
+      type: "image/png",
+    },
+    {
+      src: "/icon-144x144.png",
+      sizes: "144x144",
+      type: "image/png",
+    },
+    {
+      src: "/icon-152x152.png",
+      sizes: "152x152",
+      type: "image/png",
+    },
+    {
+      src: "/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/icon-256x256.png",
+      sizes: "256x256",
+      type: "image/png",
+    },
+    {
+      src: "/icon-284x284.png",
+      sizes: "284x284",
+      type: "image/png",
+    },
+    {
+      src: "/icon-384x384.png",
+      sizes: "384x384",
+      type: "image/png",
+    },
+    {
+      src: "/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
     },
   ],
 };
@@ -74,7 +98,7 @@ export default defineConfig({
         skipWaiting: true,
         sourcemap: true,
         maximumFileSizeToCacheInBytes: 3000000,
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif,webp}"],
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif,webp,json}"],
       },
     }),
   ],
