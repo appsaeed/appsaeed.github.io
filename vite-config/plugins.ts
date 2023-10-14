e953e4c56c968c5a87d03d901e2f8d4f6f510d5b
Saeed <appsaeed7@gmail.com>
2023-10-15 03:02:19 +0600

    update sitemap

--- vite-config/plugins.ts
+++ vite-config/plugins.ts
@@ -3,8 +3,20 @@ import solid from "vite-plugin-solid";
 import "./global";
 import manifest from "./manifest";
 import pwa from "./pwa";
-import SiteMap from "./sitemap";
+import sitemap from "./sitemap";
 
-const plugins: PluginOption[] = [solid() , SiteMap(), pwa({ manifest: manifest })];
+const plugins: PluginOption[] = [
+    solid(),
+    sitemap({
+        baseUrl: process.env.VITE_LIVE_URL,
+        urls: [
+            "/license",
+            "/privacy-policy",
+            '/term-and-conditions',
+            '/help-center'
+        ],
+    }),
+    pwa({ manifest: manifest })
+];
 
 export default plugins;
--- vite-config/sitemap.ts
+++ vite-config/sitemap.ts
@@ -1,16 +1,65 @@
+// import { adslash, unslash } from 'appmon/url';
+import { format } from 'date-fns'; // For formatting the last modification date
+import { writeFile } from "fs/promises";
+import { resolve } from "path";
+
+export const unslash = (str: string): string => str.replace(/(\/$)|(^\/)/g, "");
 /**
- * @package vite-plugin-sitemap
- * @link https://github.com/jbaubree/vite-plugin-sitemap
+ * add slash from last of url or string
+ * @param str
+ * @returns
  */
-import os from 'os';
-import sitemap from 'vite-plugin-sitemap';
-export default function SiteMap() {
+export const adslash = (str: string): string => unslash(str).replace(/$/, "/");
 
-    const locations = ["/license", "/privacy-policy", '/term-and-conditions', '/help-center'];
 
-    return sitemap({
-        dynamicRoutes: locations,
-        hostname: os.hostname(),
-        basePath: 'https://appsaeed.github.io'
-    });
+type SitemapOptions = {
+    baseUrl?: string;
+    outputDir?: string;
+    urls?: string[];
+    filename?: string;
 }
+export default function sitemap(options?: SitemapOptions) {
+    const urls = options.urls || [];
+    const baseUrl = options.baseUrl || "";
+    const outputDir = options.outputDir || "";
+    const filename = options.filename || 'sitemap.xml';
+
+    const today = new Date();
+    const formattedDate = format(today, "yyyy-MM-dd'T'HH:mm:ss'Z'"); // Format the current date
+
+    const sitemapEntries = urls.map((route, i) => {
+        return `<url>
+            <loc>${adslash(baseUrl)}${unslash(route)}</loc>
+            <lastmod>${formattedDate}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>Medium Priority</priority>
+        </url>`;
+    });
+
+    const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+        <url>
+            <loc>${baseUrl}</loc>
+            <lastmod>${formattedDate}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>High Priority</priority>
+        </url>
+        ${sitemapEntries.join('')}
+    </urlset>
+    `;
+
+    return {
+
+        name: 'sitemap',
+
+        async writeBundle(options) {
+            try {
+                await writeFile(resolve(options.dir, filename), sitemapXML);
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    };
+};
