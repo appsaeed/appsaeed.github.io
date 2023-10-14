4b1c4acc118b28fe7f313f8b051e7edd41ae0e66
Saeed <appsaeed7@gmail.com>
2023-10-15 02:00:35 +0600

    update sitemap

--- vite-config/sitemap.ts
+++ vite-config/sitemap.ts
@@ -2,10 +2,15 @@
  * @package vite-plugin-sitemap
  * @link https://github.com/jbaubree/vite-plugin-sitemap
  */
+import os from 'os';
 import sitemap from 'vite-plugin-sitemap';
-export default function SiteMap(){
+export default function SiteMap() {
 
-const locations = ["/license" ,"/privacy-policy",'/term-and-conditions','/help-center'];
+    const locations = ["/license", "/privacy-policy", '/term-and-conditions', '/help-center'];
 
-return sitemap( {  dynamicRoutes: locations , hostname: 'appsaeed.github.io'    });
+    return sitemap({
+        dynamicRoutes: locations,
+        hostname: os.hostname(),
+        basePath: 'https://appsaeed.github.io'
+    });
 }
